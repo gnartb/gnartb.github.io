3648dee364fa62399cd8b1aeb467700d0dc46c2b
Trang Bui <thuytrang0329@gmail.com>
2024-12-07 16:27:12 +0800

    getImages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import { getImages } from '@utils/getImages.js';
-
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
 	site: 'https://gnartb.github.io',
-	resolve: {
-		alias: {
-		  '@utils': './src/utils', // Optional: Define a custom path alias
-		},
-	  },
 });
 
 
