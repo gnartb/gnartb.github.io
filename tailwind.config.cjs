ffec5134c982d2551143712fffe41242f921e308
Trang Bui <thuytrang0329@gmail.com>
2024-12-31 18:48:49 +0700

    case studies and theme update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,45 +5,46 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				bgColor: colors.zinc[950],
-				bodyColor: colors.zinc[300],
-				subColor: colors.slate[300],
-				sectionTitle: colors.slate[500],
-				urlColor: colors.slate[400],
-				// article
-				aBgColor: colors.zinc[50],
-				dbodyColor: colors.zinc[900],
-				dparagraphColor: colors.zinc[700],
-				dsubColor: colors.slate[400],
-				issue: colors.pink[700],
-				solution: colors.green[600]
-			},
-			fontFamily: {
-				bodyFont: 'Be Vietnam Pro, san-serif',
-				headLineFont: 'Suranna',
-				articleHeadlineFont: 'Geist, san-serif',
-				urlFont: 'Libre Franklin',
+				//background
+				'dark': {
+					background: colors.zinc[900],
+					heading: colors.zinc[200],
+					body: colors.zinc[300],
+					subtle: colors.zinc[500],
+					footnote: colors.zinc[500],
+					// warning:
+					// success:
 
-				//for article
-				articleBodyFont: 'Inter, san-serif',
-				articleHeadlineFont: 'Geist, san-serif',
+				},
+
+				'light': {
+					background: colors.zinc[50],
+					heading: colors.zinc[200],
+					body: colors.zinc[800],
+					subtle: colors.zinc[500],
+					subtlest: colors.zinc[400],
+					footnote: colors.zinc[500],
+					test: colors.lime[400],
+					warning: colors.rose[500],
+					// success:
+				},
+
 			},
+
+			fontFamily: {
+				body: 'Be Vietnam Pro, san-serif',
+				heading: 'Suranna',
+				url: 'Libre Franklin',
+			},
+
 			fontSize: {
 				// apply for general body text
-				bodyFontSize: '14px',
-				subFontSize: '12px',
-				urlFontSize: '14px',
-				// use for coase studies
-				articleHead: '20px',
-				articleBodyFontsize: '14px',
-				articleSubFontSize: '12px',
-				h1: '24px',
-				h2: '22px',
-				h5: '16px',
-				h6: '14px',
-				h7: '12px'
-
+				heading: "20px",
+				body: '14px',
+				subtlest: '13px',
+				url: '14px',
 			},
+
 			gap: {
 				// apply for gap
 				pGap: '22.4px',
@@ -52,165 +53,218 @@ module.exports = {
 
 				// apply for article
 				aSectionContentGap: '12px',
-				paragraphGap: '14px',
+				paragraphGap: '16px',
 
 			},
 
 		}
 	},
+
+	
 	plugins: [
 		function ({ addUtilities, theme }) {
 			addUtilities({
-				'.bodyParagraph': {
-					fontFamily: theme('fontFamily.bodyFont'),
-					fontSize: theme('fontSize.bodyFontSize'),
+				'.typo-heading-large': {
+					fontFamily: theme('fontFamily.heading'),
+					fontSize: theme('fontSize.heading'),
 					fontWeight: theme('fontWeight.light'),
-					color: theme('colors.bodyColor'),
 					lineHeight: '22.4px',
 				},
-				'.subParagraph': {
-					fontFamily: theme('fontFamily.bodyFont'),
-					fontSize: theme('fontSize.subFontSize'),
-					fontWeight: theme('fontWeight.light'),
-					color: theme('colors.subColor'),
+
+				'.typo-heading-medium': {
+					fontFamily: theme('fontFamily.body'),
+					fontSize: '15px',
+					fontWeight: theme('fontWeight.semibold'),
 					lineHeight: '22.4px',
 				},
-				'.bodyBold': {
-					fontFamily: theme('fontFamily.bodyFont'),
-					fontSize: theme('fontSize.bodyFontSize'),
-					fontWeight: theme('fontWeight.bold'),
-					color: theme('colors.bodyColor'),
+
+
+				'.typo-body': {
+					fontFamily: theme('fontFamily.body'),
+					fontSize: theme('fontSize.body'),
+					fontWeight: theme('fontWeight.light'),
+					lineHeight: '22.4px',
 				},
-				'.bodyLink': {
+
+				'.typo-subtlest': {
+					fontFamily: theme('fontFamily.body'),
+					fontSize: theme('fontSize.subtlest'),
+					fontWeight: theme('fontWeight.light'),
+					lineHeight: '22.4px',
+				},
+
+				'.typo-tooltip': {
+					fontFamily: theme('fontFamily.body'),
+					fontSize: theme('fontSize.subtlest'),
+					fontWeight: theme('fontWeight.light'),
+					lineHeight: '22.4px',
+					textAlign: 'center',
+				},
+
+
+				'.typo-link': {
 					borderBottomWidth: '1px',
-					borderColor: colors.zinc[700],
+					borderColor: colors.zinc[400],
 					// transition: 'all 0.3s ease',
-
 				},
-				'.bodyLink:hover': {
+
+				'.typo-link:hover': {
 					borderBottomWidth: '1px',
-					borderColor: colors.zinc[300],
+					borderColor: colors.zinc[500],
 					'background-color': colors.lime[400],
-					color: colors.black,
 				},
 
-				'.menuText': {
-					fontFamily: theme('fontFamily.urlFont'),
-					color: theme('colors.subColor'),
+				'.typo-red': {
+					color: theme('colors.light.warning'),
+					fontStyle: 'italic',
+				},
 
+
+				'.typo-topMenu': {
+					color: theme('colors.light.body'),
 
 				},
-				'.menuText:hover': {
+
+				'.typo-topMenu:hover': {
 					'background-color': colors.lime[400],
-					color: colors.black,
 				},
 
-				'.subText': {
-					color: theme('colors.subColor'),
-					fontSize: theme('fontSize.subFontSize'),
-				},
+				
 
-				'.darkSubText': {
-					color: theme('colors.dsubColor'),
-					fontSize: theme('fontSize.subFontSize'),
-				},
-				'.urlText': {
-					color: theme('colors.urlColor'),
-					fontSize: theme('fontSize.urlFontSize'),
-					fontFamily: theme('fontFamily.urlFont'),
-					fontWeight: '300',
-				},
-				'.sectionTitle': {
-					fontFamily: theme('fontFamily.urlFont'),
-					fontSize: theme('fontSize.bodyFontSize'),
-					fontWeight: theme('fontWeight.light'),
-					color: theme('colors.sectionTitle'),
-				},
+
+
+
+				// '.subParagraph': {
+				// 	fontFamily: theme('fontFamily.bodyFont'),
+				// 	fontSize: theme('fontSize.subFontSize'),
+				// 	fontWeight: theme('fontWeight.light'),
+				// 	lineHeight: '22.4px',
+				// },
+				// '.bodyBold': {
+				// 	fontFamily: theme('fontFamily.bodyFont'),
+				// 	fontSize: theme('fontSize.bodyFontSize'),
+				// 	fontWeight: theme('fontWeight.bold'),
+				// },
+
+
+
+
+
+				// '.menuText': {
+				// 	fontFamily: theme('fontFamily.urlFont'),
+				// 	color: theme('colors.subColor'),
+
+
+
+
+				// '.subText': {
+				// 	color: theme('colors.subColor'),
+				// 	fontSize: theme('fontSize.subFontSize'),
+				// },
+
+				// '.darkSubText': {
+				// 	color: theme('colors.dsubColor'),
+				// 	fontSize: theme('fontSize.subFontSize'),
+				// },
+				// '.urlText': {
+				// 	color: theme('colors.urlColor'),
+				// 	fontSize: theme('fontSize.urlFontSize'),
+				// 	fontFamily: theme('fontFamily.urlFont'),
+				// 	fontWeight: '300',
+				// },
+				// '.sectionTitle': {
+				// 	fontFamily: theme('fontFamily.urlFont'),
+				// 	fontSize: theme('fontSize.bodyFontSize'),
+				// 	fontWeight: theme('fontWeight.light'),
+				// 	color: theme('colors.sectionTitle'),
+				// },
+
 
 
 
 				// for article
 
-				'.articleHead': {
-					fontFamily: theme('fontFamily.articleHead'),
-					fontSize: theme('fontSize.articleHead'),
-					fontWeight: theme('fontWeight.bold'),
-					color: theme('colors.dbodyColor'),
-					lineHeight: '22.4px',
-				},
-				'.articleBodyParagraph': {
-					fontFamily: theme('fontFamily.articleBodyFont'),
-					fontSize: theme('fontSize.articleBodyFontsize'),
-					fontWeight: theme('fontWeight.light'),
-					color: theme('colors.dparagraphColor'),
-					lineHeight: '20px',
-				},
+				// '.articleHead': {
+				// 	fontFamily: theme('fontFamily.articleHead'),
+				// 	fontSize: theme('fontSize.articleHead'),
+				// 	fontWeight: theme('fontWeight.bold'),
+				// 	color: theme('colors.dbodyColor'),
+				// 	lineHeight: '22.4px',
+				// },
+				// '.articleBodyParagraph': {
+				// 	fontFamily: theme('fontFamily.articleBodyFont'),
+				// 	fontSize: theme('fontSize.articleBodyFontsize'),
+				// 	fontWeight: theme('fontWeight.light'),
+				// 	color: theme('colors.dparagraphColor'),
+				// 	lineHeight: '20px',
+				// },
 
-				'.articleSubParagraph': {
-					fontFamily: theme('fontFamily.articleBodyFont'),
-					fontSize: theme('fontSize.articleSubFontSize'),
-					fontWeight: theme('fontWeight.light'),
-					color: theme('colors.dColor'),
-					lineHeight: '18px',
-				},
+				// '.articleSubParagraph': {
+				// 	fontFamily: theme('fontFamily.articleBodyFont'),
+				// 	fontSize: theme('fontSize.articleSubFontSize'),
+				// 	fontWeight: theme('fontWeight.light'),
+				// 	color: theme('colors.dColor'),
+				// 	lineHeight: '18px',
+				// },
 
-				'.aLabel': {
-					fontFamily: theme('fontFamily.articleBodyFont'),
-					fontSize: theme('fontSize.articleSubFontSize'),
-					fontWeight: theme('fontWeight.light'),
-					color: theme('colors.dsubColor'),
-					lineHeight: '22.4px',
-				},
+				// '.aLabel': {
+				// 	fontFamily: theme('fontFamily.articleBodyFont'),
+				// 	fontSize: theme('fontSize.articleSubFontSize'),
+				// 	fontWeight: theme('fontWeight.light'),
+				// 	color: theme('colors.dsubColor'),
+				// 	lineHeight: '22.4px',
+				// },
 
-				'.articleH2': {
-					fontFamily: theme('fontFamily.articleBodyFont'),
-					fontSize: theme('fontSize.h2'),
-					fontWeight: theme('fontWeight.light'),
-					color: theme('colors.dbodyColor'),
-					lineHeight: '22.4px',
-					textAlign: 'center',
-				},
+				// '.articleH2': {
+				// 	fontFamily: theme('fontFamily.articleBodyFont'),
+				// 	fontSize: theme('fontSize.h2'),
+				// 	fontWeight: theme('fontWeight.light'),
+				// 	color: theme('colors.dbodyColor'),
+				// 	lineHeight: '22.4px',
+				// 	textAlign: 'center',
 
-				'.articleH5': {
-					fontFamily: theme('fontFamily.articleBodyFont'),
-					fontSize: theme('fontSize.h5'),
-					fontWeight: theme('fontWeight.bold'),
-					color: theme('colors.dbodyColor'),
-					lineHeight: '22.4px',
-				},
-				
-				// use for tooltip text below image
-				'.articleH6': {
-					fontFamily: theme('fontFamily.articleBodyFont'),
-					fontSize: theme('fontSize.h6'),
-					fontWeight: theme('fontWeight.light'),
-					color: theme('colors.dsubColor'),
-					fontStyle: 'italic',
-					textAlign: 'center',
-				},
+				// },
 
-				'.articleH7': {
-					fontFamily: theme('fontFamily.articleBodyFont'),
-					fontSize: theme('fontSize.h7'),
-					fontWeight: theme('fontWeight.light'),
-					color: theme('colors.dsubColor'),
-				},
+				// '.articleH5': {
+				// 	fontFamily: theme('fontFamily.articleBodyFont'),
+				// 	fontSize: theme('fontSize.h5'),
+				// 	fontWeight: theme('fontWeight.bold'),
+				// 	color: theme('colors.dbodyColor'),
+				// 	lineHeight: '22.4px',
+				// },
 
-				'.articleSemibold': {
-					fontWeight: theme('fontWeight.semibold'),
-					color: theme('colors.dbodyColor'),
-				},
+				// // use for tooltip text below image
+				// '.articleH6': {
+				// 	fontFamily: theme('fontFamily.articleBodyFont'),
+				// 	fontSize: theme('fontSize.h6'),
+				// 	fontWeight: theme('fontWeight.light'),
+				// 	color: theme('colors.dsubColor'),
+				// 	fontStyle: 'italic',
+				// 	textAlign: 'center',
+				// },
 
-				'.issue': {
-					fontWeight: theme('fontWeight.light'),
-					fontStyle: 'italic',
-					color: theme('colors.issue'),
-				},
-				'.solution': {
-					fontWeight: theme('fontWeight.semibold'),
-					// fontStyle: 'italic',
-					color: theme('colors.dbodyColor'),
-				},
+				// '.articleH7': {
+				// 	fontFamily: theme('fontFamily.articleBodyFont'),
+				// 	fontSize: theme('fontSize.h7'),
+				// 	fontWeight: theme('fontWeight.light'),
+				// 	color: theme('colors.dsubColor'),
+				// },
+
+				// '.articleSemibold': {
+				// 	fontWeight: theme('fontWeight.semibold'),
+				// 	color: theme('colors.dbodyColor'),
+				// },
+
+				// '.issue': {
+				// 	fontWeight: theme('fontWeight.light'),
+				// 	fontStyle: 'italic',
+				// 	color: theme('colors.issue'),
+				// },
+				// '.solution': {
+				// 	fontWeight: theme('fontWeight.semibold'),
+				// 	// fontStyle: 'italic',
+				// 	color: theme('colors.dbodyColor'),
+				// },
 
 
 
